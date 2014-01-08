68c4131d6bb3752b18aa320dc4c29cc7677399dd
Steve Saarinen <ssaarinen@whitepages.com>
2014-01-07 16:50:45 -0800

    Remove byebug and bump version

--- lib/abtest/processor.rb
+++ lib/abtest/processor.rb
@@ -5,7 +5,6 @@ module Abtest
     def self.process_tests controller
       Abtest.abtest_config.registered_tests.each do |test_hash|
         if (test_hash[:check].call(controller.request))
-          byebug
           controller.prepend_view_path(test_hash[:prefix])
         end
       end
--- lib/abtest/version.rb
+++ lib/abtest/version.rb
@@ -1,3 +1,3 @@
 module Abtest
-  VERSION = "0.0.1"
+  VERSION = "0.0.2"
 end
