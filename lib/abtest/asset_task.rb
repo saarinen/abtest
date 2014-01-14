0d067af7c076b38c7a7f3c6ad9338e767271aade
Steve Saarinen <ssaarinen@whitepages.com>
2014-01-14 12:49:50 -0800

    Use independent sprockets env for precompile

--- lib/abtest/asset_task.rb
+++ lib/abtest/asset_task.rb
@@ -25,22 +25,32 @@ module Abtest
             images_path           = File.join(experiment_path, app.config.assets.prefix, 'images')
             javascript_path       = File.join(experiment_path, app.config.assets.prefix, 'javascript')
 
+            # Create a custom sprockets environment
+            experiment_environment = Sprockets::Environment.new(Rails.root.to_s) do |env|
+              env.context_class.class_eval do
+                include ::Sprockets::Rails::Helper
+                assets_prefix = File.join('experiments', name)
+              end
+            end
+
             # Always calculate digests and compile files
-            app.config.assets.digest  = true
-            app.config.assets.compile = true
-            environment.cache         = :null_store  # Disables the Asset cache
+            app.config.assets.digest      = true
+            app.config.assets.compile     = true
+            experiment_environment.cache  = :null_store  # Disables the Asset cache
 
-            environment.prepend_path("#{application_css_path}")
-            environment.prepend_path("#{images_path}")
-            environment.prepend_path("#{javascript_path}")
+            experiment_environment.prepend_path("#{application_css_path}")
+            experiment_environment.prepend_path("#{images_path}")
+            experiment_environment.prepend_path("#{javascript_path}")
 
-            # Set view cotext for asset path
-            environment.context_class.assets_prefix = File.join(app.config.assets.prefix, 'experiments', name)
+            # Copy config.assets.paths to Sprockets
+            app.config.assets.paths.each do |path|
+              experiment_environment.append_path path
+            end
 
             output_file = File.join(app.root, 'public', app.config.assets.prefix, 'experiments', name)
             app.config.assets.prefix = "/experiments/single_colunm/assets"
 
-            manifest = Sprockets::Manifest.new(environment, output_file)
+            manifest = Sprockets::Manifest.new(experiment_environment, output_file)
 
             with_logger do
               manifest.compile(assets)
